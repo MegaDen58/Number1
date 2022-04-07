fe0e963209465c9a0a5982ef66ae0d367d20bad3
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-04-07 19:58:19 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,20 +8,8 @@ public class Main {
         String result = "";;
         System.out.print("Введите предложение: ");
         String word = in.nextLine();
-        int max = word.length();
-        String[] words = word.split(" ");
-        for(String t : words){
-            int c = t.length();
-            if(c < max){
-                max = c;
-            }
-        }
-        System.out.printf("Длина минимально слова равна %d символа(ов)\nЭти слова: \n", max);
-        for(String t : words){
-            if(t.length() == max){
-                result = t;
-                System.out.println(result);
-            }
-        }
+        int lastIndex = word.length() - 1;
+        char[] words = word.toCharArray();
+        System.out.println(words[lastIndex]);
     }
 }
